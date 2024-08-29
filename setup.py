a332547fe8fe1aa0a61df779bc7996825f1a00a5
Jose Castillo <jcastillo@redhat.com>
2024-08-29 15:04:50 +0100

    Set up sos-next
    Signed-off-by: Jose Castillo <jcastillo@redhat.com>

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from sos import __version__ as VERSION
+
+
+setup(
+    name='sos',
+    version=VERSION,
+    # to avoid a packaging dependency on older RHELs
+    # we only declare it on recent Python versions
+    install_requires=['pexpect', 'pyyaml', 'packaging;python_version>="3.11"'],
+    description=(
+        'A set of tools to gather troubleshooting information from a system'
+    ),
+    author='Bryn M. Reeves',
+    author_email='bmr@redhat.com',
+    maintainer='Jake Hunsaker',
+    maintainer_email='jhunsake@redhat.com',
+    url='https://github.com/sosreport/sos',
+    license="GPLv2+",
+    scripts=['bin/sos', 'bin/sosreport', 'bin/sos-collector'],
+    data_files=[
+        ('share/man/man1', ['man/en/sosreport.1', 'man/en/sos-report.1',
+                            'man/en/sos.1', 'man/en/sos-collect.1',
+                            'man/en/sos-collector.1', 'man/en/sos-clean.1',
+                            'man/en/sos-mask.1', 'man/en/sos-help.1']),
+        ('share/man/man5', ['man/en/sos.conf.5']),
+        ('share/licenses/sos', ['LICENSE']),
+        ('share/doc/sos', ['AUTHORS', 'README.md']),
+        ('config', ['sos.conf', 'tmpfiles/tmpfilesd-sos-rh.conf'])
+    ],
+    packages=find_packages(include=['sos', 'sos.*'])
+)
+
+# vim: set et ts=4 sw=4 :
---
+++ sos/report/plugins/instructlab.py
@@ -0,0 +1,47 @@
+# Copyright (C) 2024 Red Hat, Inc., Jose Castillo <jcastillo@redhat.com>
+
+# This file is part of the sos project: https://github.com/sosreport/sos
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# version 2 of the GNU General Public License.
+#
+# See the LICENSE file in the source distribution for further information.
+
+from sos.report.plugins import Plugin, IndependentPlugin
+
+
+class Instructlab(Plugin, IndependentPlugin):
+
+    short_desc = 'Instructlab'
+    plugin_name = 'instructlab'
+    profiles = ('ai',)
+    containers = ('instructlab', 'ilab',)
+
+    def setup(self):
+        config_dir = '/usr/share/instructlab/config/'
+        opt_dir = '/opt/app-root/src/.config/instructlab/'
+
+        instructlab_con = None
+
+        subcmds = [
+            'taxonomy diff',
+            'taxonomy diff --taxonomy-base=empty',
+            'system info',
+            'model list',
+        ]
+
+        for con in self.containers:
+            if self.get_container_by_name(con):
+                instructlab_con = con
+                self.add_copy_spec([f'{config_dir}rhel_ai_config.yaml',
+                                    f'{opt_dir}config.yaml'],
+                                   container=instructlab_con)
+                self.add_cmd_output(
+                    [f"ilab {sub}" for sub in subcmds],
+                    container=instructlab_con
+                )
+                self.add_container_logs(instructlab_con)
+
+
+# vim: set et ts=4 sw=4 :
